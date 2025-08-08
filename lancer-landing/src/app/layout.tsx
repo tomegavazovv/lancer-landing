f63f2f82c2a1d0b53221877e9dcba493a5f736d9
iv401 <ivan@mvpmasters.com>
2025-08-08 16:56:11 +0200

    Add RB2B tracking script to layout

--- lancer-landing/src/app/layout.tsx
+++ lancer-landing/src/app/layout.tsx
@@ -127,6 +127,18 @@ export default function RootLayout({
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}
         </Script>
+        <Script id="rb2b-init" strategy="afterInteractive">
+          {`
+            !function(key) {
+              if (window.reb2b) return;
+              window.reb2b = {loaded: true};
+              var s = document.createElement("script");
+              s.async = true;
+              s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";
+              document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);
+            }("Z6PVLHPMY06R");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
